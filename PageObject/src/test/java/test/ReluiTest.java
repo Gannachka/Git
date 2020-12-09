18c33a5921b61f75a8be640935b47464e049b70e
Hanna Miki <immeditate@gmail.com>
2020-12-09 20:04:18 +0300

    kkk

---
+++ PageObject/src/test/java/test/ReluiTest.java
@@ -0,0 +1,59 @@
+package test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import page.ReluiPage;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ReluiTest {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void BrowserSetup(){
+        System.setProperty("webdriver.chrome.driver","/C:/webdrivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @BeforeEach
+    public void clearCookies(){
+        //Удалил всё куки
+        driver.manage().deleteAllCookies();
+    }
+
+    @Test
+    void AddingItemsToCartTest(){
+        WebElement itemName = new ReluiPage(driver)
+                .openPage()
+                .addToCart()
+                .goToCart()
+                .getNameOfFirstItem();
+        assertTrue(itemName.isDisplayed());
+    }
+
+    @Test
+    void ChooseColorAndAddToCartTest(){
+        WebElement itemColor=new ReluiPage(driver)
+                .openPage()
+                .goToProduct()
+                .chooseColor()
+                .addToBag()
+                .goToBag()
+                .getProductColor();
+        assertTrue(itemColor.isDisplayed());
+    }
+
+    @AfterAll
+    static void driverShutDown(){
+        driver.quit();
+        driver=null;
+    }
+}
